d10efe7882ef95fc1021ff78d15db6d15aa127d6
Joshua Vayer <joshua.vayer@gmail.com>
2022-03-17 23:39:20 +0100

    2.5: separate module

--- part2/courseinfo/src/components/Total/index.js
+++ part2/courseinfo/src/components/Total/index.js
@@ -2,10 +2,9 @@ import React from 'react'
 
 const Total = ({parts}) => {
   const getTotal = () => {
-    let total = 0;
-    parts.forEach(part => {
-      total = total + part.exercises
-    });
+    let total = parts.reduce((acc, part) => {
+      return acc + part.exercises
+    }, 0)
     return total;
   }
 
